9427862ac20fee7ae073f5696e458baa63e56845
bugbit <ohernandezba@gmail.com>
2019-01-14 15:37:49 +0100

    Is primer

--- Algebra/Math/Algorithms.cs
+++ Algebra/Math/Algorithms.cs
@@ -8,6 +8,23 @@ namespace Algebra.Math
 {
     public static class Algorithms
     {
+        /* This function calculates (ab)%c */
+        public static int Module(dynamic a, dynamic b, dynamic c)
+        {
+            dynamic x = a / a, y = a; // long long is taken to avoid overflow of intermediate results
+
+            while (b > 0)
+            {
+                if (b % 2 == 1)
+                {
+                    x = (x * y) % c;
+                }
+                y = (y * y) % c; // squaring the base
+                b /= 2;
+            }
+
+            return x % c;
+        }
         public static bool IsPrime(dynamic argNumber)
         {
             if ((argNumber % 2) == 0)
