9a18aca7b3bcc68e78c4f3130acd919402b38c28
bugbit <ohernandezba@gmail.com>
2019-02-19 15:36:39 +0100

    Numeros primos

---
+++ Algebra/Algebra/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algebra.Models
+{
+    public enum EMenu
+    {
+        TeorimaDeLosNumeros,
+        EsNumeroPrimo
+    }
+}
