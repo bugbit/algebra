37e4c04fb2e5af4897760fa2e1634fc4a08d9bd2
bugbit <ohernandezba@gmail.com>
2019-02-26 15:41:22 +0100

    addview

---
+++ Algebra/Algebra/Core/ResString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Text;
+
+namespace Algebra.Core
+{
+    public sealed class ResString
+    {
+        public ResString() { }
+        public ResString(Func<CultureInfo> getCulture, Func<ResourceManager> getResourceManage, string key)
+        {
+            GetCulture = getCulture;
+            GetResourceManager = getResourceManage;
+        }
+
+        public Func<CultureInfo> GetCulture { get; set; }
+        public Func<ResourceManager> GetResourceManager { get; set; }
+        public string Key { get; set; }
+
+        public override string ToString() => GetResourceManager.Invoke().GetString(Key, GetCulture.Invoke());
+    }
+}
