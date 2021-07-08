223829ccafbcb309e6c6e99a67a9f54e545ebf9e
bugbit <ohernandezba@gmail.com>
2021-07-08 23:56:44 +0200

    prime

---
+++ Algebra/Algebra.App/Pages/PrimeFactorsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algebra.App.Pages
+{
+    public class PrimeFactorsModel
+    {
+        [Required]
+        public string NumberSingleOrList { get; set; }
+        public int Method { get; set; }
+    }
+}
